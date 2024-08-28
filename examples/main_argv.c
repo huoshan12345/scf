e837f3a4fa9a11350cca0ce8e147787c3b4f6fdb
yu.dongliang <18588496441@163.com>
2024-08-28 16:56:16 +0800

    support argc & argv in main() for x64, support fopen(), fclose(), ...

---
+++ examples/main_argv.c
@@ -0,0 +1,11 @@
+
+int printf(const char* fmt, ...);
+
+int main(int argc, char** argv, char** envp)
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return 0;
+}
--- lib/_start.s
+++ lib/_start.s
@@ -2,8 +2,19 @@
 .global _start, main
 
 _start:
+	mov  %rsp, %rsi
+	add  $8,   %rsi   # argv
+	mov  %rsi, %rdx
+1:
+	mov  (%rdx), %rdi
+	add  $8,    %rdx  # envp
+	test %rdi,  %rdi
+	jnz  1b
+
+	mov  (%rsp), %rdi # argc
+
 	call main
 	mov  %rax, %rdi
 	mov  $60,  %rax
 	syscall
-.fill 7, 1, 0
+.fill 5, 1, 0
